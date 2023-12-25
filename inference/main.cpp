000d80c2e2ac41c902d3bbbe2cd11deab5025252
Ulrik Isdahl <ulrikyi@stud.ntnu.no>
2023-12-25 15:24:14 +0100

    bug: add parameter + small refactor

--- inference/main.cpp
+++ inference/main.cpp
@@ -6,33 +6,7 @@
 #include <memory>
 using namespace std;
 
-Eigen::MatrixXf loadData(){
-    string pathToData = "/Users/ulrikisdahl/Desktop/machine learning/mnist/trainingSet/trainingSet/0";
-    int BATCH_SIZE = 8;  
-
-    vector<cv::String> fn;
-    cv::glob("/Users/ulrikisdahl/Desktop/machine learning/mnist/trainingSet/trainingSet/0/*jpg", fn, false);
-    cout << fn.size() << endl;
-    cout << fn[1] << endl;
-
-    Eigen::MatrixXf batchMatrix(BATCH_SIZE, 8*8);
-    for (int i = 0; i < BATCH_SIZE; i++){
-        cv::Mat img = cv::imread(fn[i], cv::IMREAD_GRAYSCALE);
-        cv::Mat resized;
-        cv::resize(img, resized, cv::Size(8,8));
-
-        Eigen::VectorXf eigenImage(8*8); //one dimensional vector with 64 elements
-        for (int i = 0; i < resized.rows; i++){
-            for (size_t j = 0; j < resized.cols; j++){
-                eigenImage[i*8 + j] = resized.at<uchar>(i,j);
-            }
-        }
-        batchMatrix.row(i) = eigenImage;
-    }
-
-    return batchMatrix;
-};  
-
+Eigen::MatrixXf loadData();
 
 
 int main(){
@@ -61,12 +35,36 @@ int main(){
     test << 0, 0, 0, 0, 0, 0, 0, 0; 
     cout << test.rows() << endl;
 
-    model.fit(data, test, 0.001f);
-
-    //compute loss
-
-    //use loss to move backwards and compute gradients (backprop)
+    model.fit(data, test, 6, 0.001f);
 
     cout << "main working" << endl;
     return 0;
 }
+
+
+Eigen::MatrixXf loadData(){
+    string pathToData = "/Users/ulrikisdahl/Desktop/machine learning/mnist/trainingSet/trainingSet/0";
+    int BATCH_SIZE = 8;  
+
+    vector<cv::String> fn;
+    cv::glob("/Users/ulrikisdahl/Desktop/machine learning/mnist/trainingSet/trainingSet/0/*jpg", fn, false);
+    cout << fn.size() << endl;
+    cout << fn[1] << endl;
+
+    Eigen::MatrixXf batchMatrix(BATCH_SIZE, 8*8);
+    for (int i = 0; i < BATCH_SIZE; i++){
+        cv::Mat img = cv::imread(fn[i], cv::IMREAD_GRAYSCALE);
+        cv::Mat resized;
+        cv::resize(img, resized, cv::Size(8,8));
+
+        Eigen::VectorXf eigenImage(8*8); //one dimensional vector with 64 elements
+        for (int i = 0; i < resized.rows; i++){
+            for (size_t j = 0; j < resized.cols; j++){
+                eigenImage[i*8 + j] = resized.at<uchar>(i,j);
+            }
+        }
+        batchMatrix.row(i) = eigenImage;
+    }
+
+    return batchMatrix;
+};  
